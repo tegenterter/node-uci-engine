fbeeee2776f3ffeb7a61fade7812eddfe15b4365
Thibaut Vandenbussche <thibaut.vandenbussche@gmail.com>
2025-12-17 11:46:46 +0100

    Expand depth test

--- test/analysis.ts
+++ test/analysis.ts
@@ -5,17 +5,21 @@ import { Engine } from "../src/Engine/Engine";
 import { Result } from "../src/Analysis/Result";
 import { Position } from "../src/Analysis/Position";
 
-describe("Analysis", (): void => {
-    const resolution = new DepthResolution(20);
+const timeout = 10000;
+const enginePath = "./engine/stockfish-windows-x86-64-avx2";
 
+describe("Analysis", (): void => {
     it("Should find the best move Bd5 at the requested depth with the correct evaluation", (done: Function): void => {
-        const engine = new Engine("./engine/stockfish-windows-x86-64-avx2");
+        const engine = new Engine(enginePath);
         const position = new Position("r1bqkb1r/5ppp/p2ppn2/1pn5/3NP3/1BN5/PPP2PPP/R1BQR1K1 w kq - 0 1");
+        const resolution = new DepthResolution(20);
 
         engine.analyzePosition(
             position,
             resolution,
             (result: Result): void => {
+                expect(engine.isActive()).to.eq(false);
+
                 expect(result.getResolution().getProperty()).to.eq("depth");
                 expect(result.getResolution().getValue()).to.eq(resolution.getValue());
                 expect(result.getPosition().getFen()).to.eq(position.getFen());
@@ -25,24 +29,27 @@ describe("Analysis", (): void => {
                 done();
             }
         );
-    });
+    }).timeout(timeout);
 
-    it("Should find the best move Qg3 at the requested depth with the correct evaluation", (done: Function): void => {
-        const engine = new Engine("./engine/stockfish-windows-x86-64-avx2");
-        const position = new Position("5rk1/pp4pp/4p3/2R3Q1/3n4/2q4r/P1P2PPP/5RK1 b - - 0 1");
+    it("Should find the best move Bh3 at the requested depth with the correct evaluation", (done: Function): void => {
+        const engine = new Engine(enginePath);
+        const position = new Position("8/8/4kpp1/3p1b2/p6P/2B5/6P1/6K1 b - - 0 1");
+        const resolution = new DepthResolution(25);
 
         engine.analyzePosition(
             position,
             resolution,
             (result: Result): void => {
+                expect(engine.isActive()).to.eq(false);
+
                 expect(result.getResolution().getProperty()).to.eq("depth");
                 expect(result.getResolution().getValue()).to.eq(resolution.getValue());
                 expect(result.getPosition().getFen()).to.eq(position.getFen());
-                expect(result.getAnalysis().getLine().getFirstMove().getNotation()).to.eq("c3g3");
-                expect(result.getAnalysis().getEvaluation().getValue()).to.be.greaterThan(500);
+                expect(result.getAnalysis().getLine().getFirstMove().getNotation()).to.eq("f5h3");
+                expect(result.getAnalysis().getEvaluation().getValue()).to.be.greaterThan(200);
 
                 done();
             }
         );
-    });
+    }).timeout(timeout);
 });
